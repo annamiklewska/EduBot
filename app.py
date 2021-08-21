61040b17c61f21c7f39b2a30e71bd4c4be0b5f08
Anna Miklewska <takietam908@gmail.com>
2021-08-22 00:16:12 +0200

    my app

--- app.py
+++ app.py
@@ -1,18 +1,89 @@
-"""
-This bot listens to port 5002 for incoming connections from Facebook. It takes
-in any messages that the bot receives and echos it back.
-"""
-import os
-
 from flask import Flask, request
 from pymessenger.bot import Bot
 import json
+import re
+import numpy as np
+from keras.models import load_model
+import pickle as pkl
+import os
+from flask_ngrok import run_with_ngrok
 
-from chat import initialize_chatbot, reply_to
+file_name = "pickled_vars_600_pkl"
+with open(file_name, 'rb') as pf:
+    encoder_inputs, decoder_inputs, num_decoder_tokens, target_features_dict, max_decoder_seq_length, max_encoder_seq_length, num_encoder_tokens, input_features_dict, decoder_lstm, decoder_dense, reverse_target_features_dict = pkl.load(
+        pf)
 
-vocab, encoder, decoder, inv_vocab,pad_token,sos_token,eos_token,units,maxl,unicodeToAscii,normalizeString = initialize_chatbot()
+second_file_name = "final_pickle"
+with open(second_file_name, 'rb') as pf:
+    num_decoder_tokens, num_encoder_tokens, input_features_dict, max_decoder_seq_length, max_encoder_seq_length, target_features_dict = pkl.load(
+        pf)
+
+encoder_model = load_model('encoder_model_600.h5')
+decoder_model = load_model('decoder_model_600.h5')
+
+
+def decode_response(test_input):
+    # Getting the output states to pass into the decoder
+    states_value = encoder_model.predict(test_input)
+    # Generating empty target sequence of length 1
+    target_seq = np.zeros((1, 1, num_decoder_tokens))
+    # Setting the first token of target sequence with the start token
+    target_seq[0, 0, target_features_dict['<START>']] = 1.
+
+    # A variable to store our response word by word
+    decoded_sentence = ''
+
+    stop_condition = False
+
+    while not stop_condition:
+        # Predicting output tokens with probabilities and states
+        output_tokens, hidden_state, cell_state = decoder_model.predict([target_seq] + states_value)
+        # Choosing the one with highest probability
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_token = reverse_target_features_dict[sampled_token_index]
+        decoded_sentence += " " + sampled_token
+        # Stop if hit max length or found the stop token
+        if (sampled_token == '<END>' or len(decoded_sentence) > max_decoder_seq_length):
+            stop_condition = True
+        # Update the target sequence
+        target_seq = np.zeros((1, 1, num_decoder_tokens))
+        target_seq[0, 0, sampled_token_index] = 1.
+        # Update states
+        states_value = [hidden_state, cell_state]
+    return decoded_sentence
+
+
+#################
+
+class ChatBot:
+    # Method to convert user input into a matrix
+    def string_to_matrix(self, user_input):
+        tokens = re.findall(r"[\w']+|[^\s\w]", user_input)
+        user_input_matrix = np.zeros(
+            (1, max_encoder_seq_length, num_encoder_tokens),
+            dtype='float32')
+        for timestep, token in enumerate(tokens):
+            if token in input_features_dict:
+                user_input_matrix[0, timestep, input_features_dict[token]] = 1.
+        return user_input_matrix
+
+    # Method that will create a response using seq2seq model we built
+    def generate_response(self, user_input):
+        input_matrix = self.string_to_matrix(user_input)
+        chatbot_response = decode_response(input_matrix)
+        # Remove <START> and <END> tokens from chatbot_response
+        chatbot_response = chatbot_response.replace("<START>", '')
+        chatbot_response = chatbot_response.replace("<END>", '')
+        return chatbot_response
+
+
+chatbot = ChatBot()
+
 
 app = Flask(__name__)
+
+#run_with_ngrok(app)
+
 
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
@@ -31,7 +102,7 @@ def hello():
         output = request.data
         print(output)
         json_output = json.loads(output)
-        
+
         for event in json_output['entry']:
             messaging = event['messaging']
             for x in messaging:
@@ -40,19 +111,18 @@ def hello():
                     if x['message'].get('text'):
                         message = x['message']['text']
                         print("User message: " + message)
-                        #message = chatbot.generate_response(message)
-                        message = reply_to(message,vocab,encoder,decoder,inv_vocab,pad_token,sos_token,eos_token,units,maxl,unicodeToAscii,normalizeString)
+                        message = message.lower()
+                        message = chatbot.generate_response(message)
                         print("Reply: " + message)
                         print(bot.send_text_message(recipient_id, message))
                     if x['message'].get('attachments'):
                         print("pass")
 
-                        #for att in x['message'].get('attachments'):
+                        # for att in x['message'].get('attachments'):
                         #    bot.send_attachment_url(recipient_id, att['type'], att['payload']['url'])
                 else:
                     pass
         return "Success"
-
 
 
 if __name__ == "__main__":
